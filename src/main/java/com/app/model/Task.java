0614fe35f94dc61df4eb4bace7961ee58dc0f65f
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-29 20:05:26 +0530

    Created a get mapping for all tasks

--- src/main/java/com/app/model/Task.java
+++ src/main/java/com/app/model/Task.java
@@ -1,6 +1,8 @@
 package com.app.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import lombok.Data;
 @Data
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private boolean completed;

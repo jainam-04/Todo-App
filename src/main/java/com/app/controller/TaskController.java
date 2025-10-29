d8c19111d55ef606c39e99a4d21f39017b5528fe
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-29 19:35:45 +0530

    Added tasks.html

--- src/main/java/com/app/controller/TaskController.java
+++ src/main/java/com/app/controller/TaskController.java
@@ -1,11 +1,24 @@
 package com.app.controller;
 
+import com.app.model.Task;
 import com.app.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @Controller
+//@RequestMapping("/tasks")
 public class TaskController {
     @Autowired
     private TaskService service;
+
+    @GetMapping
+    public String getAllTasks(Model model){
+        List<Task> tasks = service.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
 }
--- src/main/java/com/app/service/TaskService.java
+++ src/main/java/com/app/service/TaskService.java
@@ -1,11 +1,17 @@
 package com.app.service;
 
+import com.app.model.Task;
 import com.app.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class TaskService {
     @Autowired
     private TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
 }

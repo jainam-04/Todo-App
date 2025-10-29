1e42e6aa057bc6ef92cb3baf7b2b3e466b11058f
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-29 19:25:50 +0530

    Added Task service

---
+++ src/main/java/com/app/service/TaskService.java
@@ -0,0 +1,11 @@
+package com.app.service;
+
+import com.app.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository repository;
+}
